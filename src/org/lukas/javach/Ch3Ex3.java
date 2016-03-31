bb9caeed868186a9b8a674e0c0e8c8bca0753ac8
LukasPecak <lukas.pecak@gmail.com>
2016-03-31 23:19:46 +0200

    finished 3x3

--- src/org/lukas/javach/Ch3Ex3.java
+++ src/org/lukas/javach/Ch3Ex3.java
@@ -1,10 +1,16 @@
 package org.lukas.javach;
 
 public class Ch3Ex3 {
+	static void f(Number n){
+		n.x = 0.7f;
+	}
 	
 	public static void main(String[] args){
 		Number n = new Number();
 		n.x = 0.9f;
+		System.out.println(n.x);
+		f(n);
+		System.out.println(n.x);
 		
 	}
 }
